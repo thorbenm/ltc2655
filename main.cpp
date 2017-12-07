779e39f0b6de8c3c7744e746890f614dbfe9e7eb
Thorben B. Muehleder <thorbenm@physik.uni-kassel.de>
2017-12-07 09:33:20 +0100

    piezo is now in

--- main.cpp
+++ main.cpp
@@ -5,21 +5,34 @@
 
 int main()
 {
-   int fd, result;
+   int current, piezo, result;
 
-   fd = wiringPiI2CSetup(0x10);
+   current = wiringPiI2CSetup(0x10);
+   piezo = wiringPiI2CSetup(0x12);
 
-   std::cout << "Init result: "<< fd << std::endl;
+//   std::cout << "Init result: "<< fd << std::endl;
 
-   for(int i = 0; i < 0x0000ffff; i++)
-   {
-
-     result = wiringPiI2CWriteReg16(fd, 0x0000, i) ;
+for(;;){
+for(int j = 0; j < 0x1f ; j++){
+ //    result = wiringPiI2CWriteReg16(piezo, 0x0000, j) ;
+     result = wiringPiI2CWriteReg16(current, 0x0000, j) ;
 
       if(result == -1)
       {
          std::cout << "Error.  Errno is: " << errno << std::endl;
       }
 	usleep(100000);
-   }
+}
+
+for(int j = 0x1f; j > 0 ; j--){
+//     result = wiringPiI2CWriteReg16(piezo, 0x0000, j) ;
+     result = wiringPiI2CWriteReg16(current, 0x0000, j) ;
+
+      if(result == -1)
+      {
+         std::cout << "Error.  Errno is: " << errno << std::endl;
+      }
+	usleep(100000);
+}
+}
 }
